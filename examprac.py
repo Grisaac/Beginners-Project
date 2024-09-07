4f7878072942d877daaea5dea4eec762461a9fc3
Grisaac <greatisaac245@gmail.com>
2024-09-07 12:01:37 +0100

    first commit

---
+++ examprac.py
@@ -0,0 +1,16 @@
+# my_set = {1, 2, 3}
+# my_set.remove(2)
+# # my_set.add(4)
+# # print(my_set)
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 5}
+# print(set1.intersection(set2))
+
+# x = lambda a:a+10
+# print(x(6))
+
+# mylist = [1, 2, 3, 4, 5]
+# x = mylist.copy()
+# print(x)
+
